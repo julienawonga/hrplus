e8da7ec69c5307c4b3f1c33fa5ab91eff53f9151
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-05-30 11:30:22 +0100

    spring security: disable csrf and allow post controllers

--- src/main/java/com/example/hrplus/config/SecurityConfiguration.java
+++ src/main/java/com/example/hrplus/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,6 +19,9 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests((requests) -> requests
                 // Allow all requests to the "/images" folder for your login page image
                 .requestMatchers("/images/**").permitAll()
+                .requestMatchers("/employees/add").permitAll()
+                .requestMatchers("/employees/edit").permitAll()
+
                 // Any other request requires authentication
                 .anyRequest().authenticated()
         );
@@ -25,6 +29,8 @@ public class SecurityConfiguration {
                 .loginPage("/login")
                 .permitAll());
         http.httpBasic(withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
 
